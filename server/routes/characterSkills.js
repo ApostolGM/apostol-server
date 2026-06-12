ca65a09b7a43f8c7d1a3dc4ef14248fe4e4577a5
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:12:16 +0300

    Update characterSkills.js

--- server/routes/characterSkills.js
+++ server/routes/characterSkills.js
@@ -1,4 +1,4 @@
-// server/server/routes/characterSkills.js
+// routes/characterSkills.js
 import { Router } from 'express';
 import { supabase } from '../index.js';
 import { authMiddleware } from '../middleware/auth.js';
