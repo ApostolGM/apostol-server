0616e9851912f4ea8ac9215621a8c2733c814860
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:10:16 +0300

    Update sound.js

--- server/socket/handlers/sound.js
+++ server/socket/handlers/sound.js
@@ -1,4 +1,4 @@
-// server/server/socket/handlers/sound.js
+// socket/handlers/sound.js
 export function handleSound(io, socket) {
   socket.on('sound_play', (data) => {
     socket.to(`campaign:${data.campaignId}`).emit('sound_play', data);
