fe1aa7548563d898e45878e40b94918932c504e9
ApostolGM <wetsrew@gmail.com>
2026-06-12 12:45:32 +0300

    Create scene.js

---
+++ server/socket/handlers/scene.js
@@ -0,0 +1,18 @@
+// server/server/socket/handlers/scene.js
+export function handleScene(io, socket) {
+  socket.on('scene_token_move', (data) => {
+    socket.to(`campaign:${data.campaignId}`).emit('scene_token_moved', data);
+  });
+
+  socket.on('scene_update', (data) => {
+    socket.to(`campaign:${data.campaignId}`).emit('scene_updated', data);
+  });
+
+  socket.on('scene_drawings', (data) => {
+    socket.to(`campaign:${data.campaignId}`).emit('scene_drawings', data);
+  });
+
+  socket.on('scene_portals', (data) => {
+    socket.to(`campaign:${data.campaignId}`).emit('scene_portals', data);
+  });
+}
