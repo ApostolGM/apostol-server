9bb63b7bce142971a318859fca2b34a315fc01ff
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:22:50 +0300

    Update handouts.js

--- server/routes/handouts.js
+++ server/routes/handouts.js
@@ -1,4 +1,4 @@
-// server/server/routes/handouts.js
+// routes/handouts.js
 import { Router } from 'express';
 import { supabase } from '../index.js';
 import { authMiddleware } from '../middleware/auth.js';
