9542b1b1a6c8cad1844accf7660bc7fe2eae7bfe
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:15:29 +0300

    Update items.js

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,4 +1,4 @@
-// server/server/routes/items.js
+// routes/items.js
 import { Router } from 'express';
 import { supabase } from '../index.js';
 import { authMiddleware } from '../middleware/auth.js';
