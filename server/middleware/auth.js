a00c925d3da043ccb036a82fcfe2ec0a65455f7b
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:05:27 +0300

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,5 @@
-// server/server/middleware/auth.js
+// middleware/auth.js
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../index.js';
 
 export function authMiddleware(req, res, next) {
   const header = req.headers.authorization;
@@ -8,7 +7,7 @@ export function authMiddleware(req, res, next) {
     return res.status(401).json({ error: 'Нет токена' });
   }
   try {
-    req.user = jwt.verify(header.split(' ')[1], JWT_SECRET);
+    req.user = jwt.verify(header.split(' ')[1], process.env.JWT_SECRET);
     next();
   } catch {
     res.status(401).json({ error: 'Токен недействителен' });
