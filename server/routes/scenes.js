0eb6e3719a9d717f9663ecc7944fe56da8693fcf
ApostolGM <wetsrew@gmail.com>
2026-06-12 13:18:54 +0300

    Update scenes.js

--- server/routes/scenes.js
+++ server/routes/scenes.js
@@ -1,4 +1,4 @@
-// server/server/routes/scenes.js
+// routes/scenes.js
 import { Router } from 'express';
 import { supabase } from '../index.js';
 import { authMiddleware } from '../middleware/auth.js';
